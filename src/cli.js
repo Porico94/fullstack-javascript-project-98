21779cf4b638e283844a3816d434de409cb9e79f
Porico94 <poolrimari@gmail.com>
2024-09-26 18:02:19 -0500

    Agregamos signos de interrogacion en cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greetUser = () => {
   console.log('¡Bienvenido a Brain Games!');
-  const name = readlineSync.question('Cuál es tu nombre: ');
+  const name = readlineSync.question('¿Cuál es tu nombre? ');
   console.log(`¡Hola, ${name}! `);
   return name;
 };
