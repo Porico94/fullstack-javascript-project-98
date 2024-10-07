49ed7d0f032b4b1826b4727347c7a695d5508e50
Porico94 <poolrimari@gmail.com>
2024-10-07 16:09:16 -0500

    Correción del error en el juego brain-progress

--- src/games/arithmeticProgress.js
+++ src/games/arithmeticProgress.js
@@ -2,12 +2,12 @@ export const description = '¿Qué número falta en la progresión?';
 
 export const arithmeticProgress = () => {
   let numIni = Math.ceil(Math.random() * 100 + 1);
-  const progression = Math.ceil(Math.random() * 20 + 1);
+  const progression = Math.ceil(Math.random() * 20) + 1;
   const arrayLength = Math.ceil(Math.random() * 6) + 5;
   const randomIndex = Math.ceil(Math.random() * arrayLength);
   const arrayProgress = [];
 
-  for (let i = 0; i < arrayLength; i += 1) {
+  for (let i = 0; i <= arrayLength; i += 1) {
     numIni += progression;
     arrayProgress.push(numIni);
   }
